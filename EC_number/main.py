08e32c0d606c79dae93c2cc54ae95e84c04f4d54
Yidong Song <42714970+YidongSong@users.noreply.github.com>
2024-08-01 21:21:29 +0800

    Update main.py

--- EC_number/main.py
+++ EC_number/main.py
@@ -6,6 +6,7 @@ import os
 if __name__ == '__main__':
     parser = argparse.ArgumentParser()
     parser.add_argument("--seed", type=int, default=2023)
+    parser.add_argument("--fasta", type=str, default='./EC_number/data/example.fa')
     args = parser.parse_args()
     Seed_everything(seed=args.seed)
     predict(args=args, seed=args.seed)
