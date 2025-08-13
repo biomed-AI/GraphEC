f490b3497f18168594eaf77ca249094dc7651bfc
Yidong Song <42714970+YidongSong@users.noreply.github.com>
2025-08-13 19:16:55 +0800

    Update main.py

--- EC_number/main.py
+++ EC_number/main.py
@@ -7,6 +7,8 @@ if __name__ == '__main__':
     parser = argparse.ArgumentParser()
     parser.add_argument("--seed", type=int, default=2023)
     parser.add_argument("--fasta", type=str, default='./EC_number/data/example.fa')
+    parser.add_argument("--gpu", type=int, default=None)
     args = parser.parse_args()
     Seed_everything(seed=args.seed)
+    torch.cuda.set_device(args.gpu)
     predict(args=args, seed=args.seed)
