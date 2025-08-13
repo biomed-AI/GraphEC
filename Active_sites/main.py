510a796268df4dfa92b7388729d508a82d2fc21c
Yidong Song <42714970+YidongSong@users.noreply.github.com>
2025-08-13 19:15:25 +0800

    Update main.py

--- Active_sites/main.py
+++ Active_sites/main.py
@@ -8,7 +8,9 @@ from utils import *
 if __name__ == '__main__':
     parser = argparse.ArgumentParser()
     parser.add_argument("--seed", type=int, default=2023)
+    parser.add_argument("--gpu", type=int, default=None)
     args = parser.parse_args()
     Seed_everything(seed=args.seed)
     model_class = GraphEC_AS
+    torch.cuda.set_device(args.gpu)
     predict(model_class, args)
