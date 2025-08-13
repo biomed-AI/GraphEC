e578bebfccffd45ca3e8f379bd59bf888b1d66ad
Yidong Song <42714970+YidongSong@users.noreply.github.com>
2025-08-13 19:18:21 +0800

    Update main.py

--- Optimum_pH/main.py
+++ Optimum_pH/main.py
@@ -7,7 +7,9 @@ if __name__ == '__main__':
     parser = argparse.ArgumentParser()
     parser.add_argument("--device", type=int, default=3)
     parser.add_argument("--seed", type=int, default=2023)
+    parser.add_argument("--gpu", type=int, default=None)
     args = parser.parse_args()
     Seed_everything(seed=args.seed)
+    torch.cuda.set_device(args.gpu)
     model_class = GraphEC_pH
     predict(model_class, args)
